c07106a28c1a33db00186abf24c0f8dd236ed42e
Timothy Gulstine <timothy.gulstine@gmail.com>
2025-08-29 08:11:42 -0500

    try to get bootstrap classes working

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
